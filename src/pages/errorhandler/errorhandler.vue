06456bf8e067ff2f19efbbf3bdcf87c4ecf081c0
guorong <2293018816@qq.com>
2021-03-17 20:04:24 +0800

    errorHandler

---
+++ src/pages/errorhandler/errorhandler.vue
@@ -0,0 +1,26 @@
+<template>
+<div>errorHandler</div>
+</template>
+<script>
+import Vue from 'vue'
+Vue.config.errorHandler = (err) => {
+  console.log('进来啦', err)
+}
+export default {
+  mounted () {
+    let a = null
+    if (a.length > 1) {
+    }
+  },
+
+  methods: {
+    // 收集错误上报的方法
+    collectErr (err) {
+      console.log(err, '报错啦')
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+const Index = () => import('@/pages/index')
 const Home = () => import('@/pages/home')
 const NotFound = () => import('@/pages/404')
-
+const Errorhandler = () => import('@/pages/errorhandler/errorhandler.vue')
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Home',
-      component: Home,
+      name: 'Index',
+      component: Index,
       redirect: '/home',
       children: [
         {
           path: '/home',
           name: 'Home',
           component: Home
+        },
+        {
+          path: '/errorhandler',
+          name: 'Errorhandler',
+          component: Errorhandler
         }
       ]
     },
