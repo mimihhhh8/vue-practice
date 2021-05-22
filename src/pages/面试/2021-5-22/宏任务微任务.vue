f234de33043b2d10dd527f3c651df791dd690c92
guorong <2293018816@qq.com>
2021-05-22 19:09:06 +0800

    feat(宏任务微任务): 宏任务微任务

---
+++ src/pages/面试/2021-5-22/宏任务微任务.vue
@@ -0,0 +1,34 @@
+<template>
+<div>宏任务微任务
+  <h1>关于async/await、promise和setTimeout的执行顺序</h1>
+  <!-- https://blog.csdn.net/yun_hou/article/details/88697954 -->
+</div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+
+  mounted () {
+    this.async1()
+  },
+
+  methods: {
+    async  async1 () {
+      console.log('async1 start')
+      await this.async2()
+      console.log('asnyc1 end')
+    },
+
+    async2 () {
+      console.log('async2')
+    }
+  }
+}
+</script>
+<style scoped lang="less">
+
+</style>
